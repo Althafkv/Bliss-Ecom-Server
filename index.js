52d5be010433902197cf02997ac16a9c2f856132
Althafkv <althafkvworks@gmail.com>
2023-09-11 13:09:13 +0530

    Upd

--- index.js
+++ index.js
@@ -18,10 +18,15 @@ const { notFound, errorHandler } = require('./middlewares/errorHandler')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const corsOptions ={
+    origin:'*', 
+    credentials:true, //access-control-allow-credentials:true
+     optionSuccessStatus:200,
+}
 dbConnect()
 
 app.use(morgan("dev"))
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
