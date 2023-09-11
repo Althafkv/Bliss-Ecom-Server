1bde6727e7bbfc94ecf81ea26967afad72603ac2
Althafkv <althafkvworks@gmail.com>
2023-09-11 13:38:13 +0530

    Updateed

--- index.js
+++ index.js
@@ -18,15 +18,10 @@ const { notFound, errorHandler } = require('./middlewares/errorHandler')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const corsOptions ={
-    origin:'https://bliss-admin-panel.netlify.app', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
 dbConnect()
 
 app.use(morgan("dev"))
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
