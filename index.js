c8bf529e554f923236390ecd14989a42f520b87c
Althafkv <althafkvworks@gmail.com>
2023-09-13 00:41:08 +0530

    upd

--- index.js
+++ index.js
@@ -20,25 +20,25 @@ const morgan = require('morgan')
 const cors = require('cors')
 dbConnect()
 
-app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "*")
-    res.header("Access-Control-Allow-Credentials", true)
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+// app.use((req,res,next) => {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Headers", "*")
+//     res.header("Access-Control-Allow-Credentials", true)
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
 
-    next()
-})
+//     next()
+// })
 
-app.use(cors({
-    allowedHeaders: "*",
-    allowMethods: "*",
-    origin: "*",
-}))
+// app.use(cors({
+//     allowedHeaders: "*",
+//     allowMethods: "*",
+//     origin: "*",
+// }))
 
 app.use(morgan("dev"))
-// app.use(cors({
-//     origin:'http://localhost:3000', 
-// }))
+app.use(cors({
+    origin:'http://localhost:3000', 
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
