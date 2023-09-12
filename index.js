06bcb4c57f40ee0a073e6d4af3f367788c58bfea
Althafkv <althafkvworks@gmail.com>
2023-09-12 13:22:13 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const dbConnect = require('./config/dbConnect')
 const app = express()
 require('dotenv').config()
@@ -18,17 +17,11 @@ const bodyParser = require('body-parser')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
-// const cors = require('cors')
+const cors = require('cors')
 dbConnect()
 
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST", "PUT", "DELETE"]
-    })
-)
 app.use(morgan("dev"))
-// app.use(cors())
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
