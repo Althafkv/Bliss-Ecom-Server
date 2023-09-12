e73fda7ca56013547864ae63b3f2db41bf4d4d1c
Althafkv <althafkvworks@gmail.com>
2023-09-12 10:21:57 +0530

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dbConnect = require('./config/dbConnect')
 const app = express()
-const dotenv = require('dotenv').config()
+require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const authRouter = require('./routes/authRoute')
 const productRouter = require('./routes/productRoute')
@@ -21,7 +21,10 @@ const cors = require('cors')
 dbConnect()
 
 app.use(morgan("dev"))
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -40,10 +43,11 @@ app.use('/api/upload', uploadRouter)
 app.use(notFound)
 app.use(errorHandler)
 
+// app.get("/", (req,res) => {
+//     res.setHeader("Access-Control-Allow-Credentials","true")
+//     res.send("Server Running...")
+// })
+
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
-})
-
-app.get("/", (req,res) => {
-    res.send("Server Running")
 })
