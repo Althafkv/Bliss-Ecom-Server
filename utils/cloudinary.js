86978758489c4555f694a2450ef24d5e5457ad80
Althafkv <althafkvworks@gmail.com>
2023-09-14 01:00:15 +0530

    Update

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,8 +1,11 @@
 const cloudinary = require('cloudinary')
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.SECRET_KEY
+    // cloud_name: process.env.CLOUD_NAME,
+    // api_key: process.env.API_KEY,
+    // api_secret: process.env.SECRET_KEY
+    cloud_name:'dt8flx8k4', 
+    api_key:'273484383856223',
+    api_secret:'d9bi26wcrUJulx_QQA4tyGIEjE8'
 })
 
 const cloudinaryUploadImg = async (fileToUpload) => {
