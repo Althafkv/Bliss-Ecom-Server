08aecefc6a91bee1b407c9059e20d57d7accaa62
Althafkv <althafkvworks@gmail.com>
2023-09-14 17:52:43 +0530

    Updated

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,5 +1,5 @@
-// const cloudinary = require('cloudinary')
-import {v2 as cloudinary} from 'cloudinary';
+const cloudinary = require('cloudinary')
+
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
