76b3ed66273799f6275e497b7ae84ea52594143a
Althafkv <althafkvworks@gmail.com>
2023-09-17 00:28:39 +0530

    Updated

--- index.js
+++ index.js
@@ -21,24 +21,11 @@ const cors = require('cors')
 dbConnect()
 
 app.use(morgan("dev"))
-app.use(cors())
+app.use(cors({credentials: true, origin: true}))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://bliss-ecom-server.onrender.com','http://localhost:3000', 'http://localhost:5000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    return next();
-  });
 
 app.use('/api/user', authRouter)
 app.use('/api/product', productRouter)
@@ -53,11 +40,6 @@ app.use('/api/upload', uploadRouter)
 
 app.use(notFound)
 app.use(errorHandler)
-
-// app.get("/", (req,res) => {
-//     res.setHeader("Access-Control-Allow-Credentials","true")
-//     res.send("Server Running...")
-// })
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
