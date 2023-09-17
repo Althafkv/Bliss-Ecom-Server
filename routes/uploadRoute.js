91f00676170de94b3e1de458c3cc2c8d27f7a4ae
Althafkv <althafkvworks@gmail.com>
2023-09-17 22:30:05 +0530

    Update

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const cors = require('cors')
 const { uploadImages, deleteImages } = require('../controller/uploadCtrl')
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware')
 const { uploadPhoto, productImgResize } = require('../middlewares/uploadImages')
 const router = express.Router()
 
-router.options('/', cors())
-router.post('/',cors(), authMiddleware, isAdmin, uploadPhoto.array('images', 10), productImgResize, uploadImages)
+router.post('/', authMiddleware, isAdmin, uploadPhoto.array('images', 10), productImgResize, uploadImages)
 router.delete('/delete-img/:id', authMiddleware, isAdmin, deleteImages)
 
 module.exports = router
