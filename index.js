4e3678bbc4dc8722135caf71c210464808ae9497
Althafkv <althafkvworks@gmail.com>
2023-09-17 20:40:59 +0530

    Updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use((req,res,next) => {
     res.setHeader(
         "Access-Control-Allow-Origin",
-        "http://localhost:3000"
+        "https://comfy-bonbon-ff15f1.netlify.app/"
     )
     res.setHeader(
         "Access-Control-Allow-Methods",
