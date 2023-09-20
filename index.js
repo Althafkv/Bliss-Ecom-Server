2cb19c68e8cca5a609de97d674a4f64653789f7b
Althafkv <althafkvworks@gmail.com>
2023-09-20 18:15:17 +0530

    update

--- index.js
+++ index.js
@@ -18,15 +18,27 @@ const bodyParser = require('body-parser')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
-const cors = require('cors')
+// const cors = require('cors')
 dbConnect()
 
 app.use(morgan("dev"))
-app.use(cors())
+// app.use(cors())
 
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'http://bliss-ecom-server.onrender.com', 'https://bliss-ecom-server.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/api/user', authRouter)
