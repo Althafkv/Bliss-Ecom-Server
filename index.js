a13178db916efb5418faa8e47ba26f42778ef6f6
Althafkv <althafkvworks@gmail.com>
2023-09-22 15:24:19 +0530

    update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ dbConnect()
 
 app.use(morgan("dev"))
 app.use(cors({
-    origin: ["https://bliss-ecom.netlify.app"],
+    origin: ["https://bliss-ecom.netlify.app","http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
