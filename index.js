785ba458a36636ce2a04882209bdee376d628f0b
Althafkv <althafkvworks@gmail.com>
2023-09-22 15:18:57 +0530

    Updated

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://bliss-ecom.netlify.app"); // the link of my front-end app on Netlify
+    res.setHeader("Access-Control-Allow-Origin", "https://bliss-ecom.netlify.app","http://localhost:3000");
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
